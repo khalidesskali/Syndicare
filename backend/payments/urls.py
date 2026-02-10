ec509ed6607912813f5a1f79460d99fccebd5cbf
khalidesskali <esskalikhalid7@gmail.com>
2026-02-10 22:56:18 +0100

    feat: Configure payment URLs

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from . import webhooks
+from . import views, webhooks
 
 urlpatterns = [
+    # API Endpoints
+    path('create-payment-intent/', views.CreatePaymentIntent.as_view(), name='create_payment_intent'),
+    path('payment-status/<str:payment_intent_id>/', views.PaymentStatus.as_view(), name='payment_status'),
+    path('confirm-payment/', views.confirm_payment, name='confirm_payment'),
+    
+    # Webhook
     path('webhooks/stripe/', webhooks.stripe_webhook, name='stripe_webhook'),
 ]
