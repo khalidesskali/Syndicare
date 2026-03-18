b934a1c310f77d365e293af7b3d3e18f0a65a635
khalidesskali <esskalikhalid7@gmail.com>
2026-03-18 22:53:13 +0000

    replace sqlite by postgresql

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,13 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%ui$wsmh7u*otlp01$(xlqv*%4#%1tb3s%m&gy75-oe%v(^w4n'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # Allow ngrok host
@@ -258,8 +253,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'syndicare'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
